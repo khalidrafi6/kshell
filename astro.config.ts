a546daea7b64fcca14d8205cf05fc966b7b8f0bc
خالد رافغ <khalidrafi.twzde@slmail.me>
2025-07-07 23:17:59 +0600

    fix deployment tasks

--- astro.config.ts
+++ astro.config.ts
@@ -87,9 +87,9 @@ const config = defineConfig({
       // }
     }),
   ],
-  adapter: node({
-    mode: "standalone",
-  }),
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
 });
 
 export default config;
