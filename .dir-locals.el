36ae82cbee7feeb314f89b18a278c2d1f9731ea3
خالد رافغ <khaalidrafi@gmail.com>
2026-02-27 22:58:32 +0600

    LATEST before Upgrade

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,10 @@
-((nil . ((eval . (kshell-local-keys 1)))))
+((nil . (
+         (eval .
+               (defun kshell/astro-server ()
+                 (interactive)
+                 (khalid/project-run-async-shell-command "pnpm dev" "*astro-dev*" nil (project-current nil "/home/rafi/kshell/"))))
+               
+         (khalid/project-commands . (kshell/astro-server))
+         
+         (eval .
+               (khalid/set-project-command-keybindings)))))
